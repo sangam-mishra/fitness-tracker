6b74212f21280df92345fe2a02628d7d58f2aed1
Sangam Mishra <80354784+sangam-mishra@users.noreply.github.com>
2026-08-04 23:13:39 +0530

    setting up the project

---
+++ userService /src/main/java/model/user.java
@@ -0,0 +1,39 @@
+package model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.AnyDiscriminatorImplicitValues;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name="users_tbl")
+public class user {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private int id;
+
+    @Column(unique = true,nullable = false)
+    private String userName;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String firstName;
+    private String lastName;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role=UserRole.USER;
+
+    @CreationTimestamp
+    private LocalDateTime creation_time;
+
+    @UpdateTimestamp
+    private LocalDateTime updatation_time;
+
+
+}
