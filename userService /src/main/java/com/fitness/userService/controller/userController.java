a071eee2bbd7e77247024d2596dd6545dcef69cd
Sangam Mishra <80354784+sangam-mishra@users.noreply.github.com>
2026-08-13 23:31:19 +0530

    controller added

--- userService /src/main/java/com/fitness/userService/controller/userController.java
+++ userService /src/main/java/com/fitness/userService/controller/userController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 public class userController {
 
     private userService userService;
+    private String name;
 
     @GetMapping("/{userId}")
     public ResponseEntity<UserResponse> getUser(@PathVariable String userId){
