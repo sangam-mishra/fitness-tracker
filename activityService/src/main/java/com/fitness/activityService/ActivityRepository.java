581fa7686a66d1d46f2b8f7d42e16bf406e0fe87
Sangam Mishra <80354784+sangam-mishra@users.noreply.github.com>
2026-08-23 23:25:26 +0530

    activity repository

--- activityService/src/main/java/com/fitness/activityService/ActivityRepository.java
+++ activityService/src/main/java/com/fitness/activityService/ActivityRepository.java
@@ -2,7 +2,9 @@ package com.fitness.activityService;
 
 import com.fitness.activityService.model.Activity;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ActivityRepository extends MongoRepository<Activity,String> {
 
 }
